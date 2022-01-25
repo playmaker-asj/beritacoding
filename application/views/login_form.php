0c656e9d9858691e66b23759b23d7868dec59c60
playmaker-asj <playmaker0305@gmail.com>
2022-01-25 10:30:18 +0700

    master

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -6,40 +6,39 @@
 </head>
 
 <body>
-    <?php $this->load->view('_partials/navbar.php');?>
+    <?php $this->load->view('_partials/navbar.php'); ?>
 
     <div class="container">
         <h1>Login</h1>
         <p>Masuk ke Dashboard</p>
 
-        <?php if($this->session->flashdata('message_login_error')):?>
+        <?php if ($this->session->flashdata('message_login_error')) : ?>
             <div class="invalid-feedback">
-                <?= $this->session->flashdata('message_login_error')?>
+                <?= $this->session->flashdata('message_login_error') ?>
             </div>
-            <?php endif ?>
+        <?php endif ?>
 
-            <form action="" method="post" style="max-width: 600px;">
+        <form action="" method="post" style="max-width: 600px;">
             <div>
                 <label for="name">Email/Username*</label>
-                <input type="text" name="username" class="<?= form_error('username') ? 'invalid' : '' ?>"
-                placeholder="Your username or email" value="<?= set_value('username') ?>" required />
+                <input type="text" name="username" class="<?= form_error('username') ? 'invalid' : '' ?>" placeholder="Your username or email" value="<?= set_value('username') ?>" required />
                 <div class="invalid-feedback">
                     <?= form_error('username') ?>
                 </div>
             </div>
-            <div> 
+            <div>
                 <label for="password">Password*</label>
-                <input type="password" name="password" class="<?= form_error('password') ? 'invalid' : '' ?>"
-                placeholder="Enter your password" value="<?= set_value('password')?>" required />
+                <input type="password" name="password" class="<?= form_error('password') ? 'invalid' : '' ?>" placeholder="Enter your password" value="<?= set_value('password') ?>" required />
                 <div class="invalid-feedback">
-                    <?= form_error('password')?>
+                    <?= form_error('password') ?>
                 </div>
             </div>
             <div>
                 <input type="submit" class="button button-primary" value="Login">
             </div>
-            </form>
+        </form>
     </div>
     <?php $this->load->view('_partials/footer.php'); ?>
 </body>
+
 </html>
